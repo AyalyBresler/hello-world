cab0254f6ff1195351069ea7bc103d6aa05832da
ayala <ayallag@skyvar>
2024-09-09 12:01:54 +0300

    :bug:Add * to the long exercise

--- calc/test/multiplication.test.js
+++ calc/test/multiplication.test.js
@@ -47,9 +47,9 @@ describe('MULTIPLICATION', ()=>{
         expect(result).toBe(6)
     })
     it('should return a number to string of a lot of numbers in the multi operator',()=>{
-        let calc = new Calc('4*  6*2/12-5')
+        let calc = new Calc('4*  6-12/12-5')
         let result = calc.calc()
-        expect(result).toBe(-1)
+        expect(result).toBe(18)
     })
     describe('ERROR', () => {
         it('should throw error when send only one number and operator', () => {
