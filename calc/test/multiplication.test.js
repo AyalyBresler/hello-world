0ebe22feffa36bf853241c171ea2d59fd842b450
ayala <ayallag@skyvar>
2024-09-10 10:57:32 +0300

    add the news

--- calc/test/multiplication.test.js
+++ calc/test/multiplication.test.js
@@ -47,7 +47,7 @@ describe('MULTIPLICATION', ()=>{
         expect(result).toBe(6)
     })
     it('should return a number to string of a lot of numbers in the multi operator',()=>{
-        let calc = new Calc('2-4+45+6-5')
+        let calc = new Calc('2-4+45-6+5')
         let result = calc.calc()
         expect(result).toBe(-50)
     })
