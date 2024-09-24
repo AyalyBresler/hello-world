70b3cf5670c84546533c47d763cf3de812881d5a
ayala <ayallag@skyvar>
2024-09-24 12:00:24 +0300

    add checking test

--- calc/test/op.test.js
+++ calc/test/op.test.js
@@ -36,15 +36,15 @@ describe('OPERATOR', () => {
     })
     it('should return the index of sub operator when the sub it is more than add',()=>{
         let op = new Operator('5-6+2');
-        expect(op.notContainsMultiAndContainsSubOrAdd()).toBe(1)
+        expect(op.notContainsMultiAndContainsSubOrAdd()).toBe(3)
     })
     it('should return true when + before -',()=>{
         let op = new Operator('5+6-2');
         expect(op.indexSubAndAdd()).toBe(true)
     })
-    it('should return false when - before +',()=>{
+    it('should return true when - before +',()=>{
         let op = new Operator('5-6+2');
-        expect(op.indexSubAndAdd()).toBe(false)
+        expect(op.indexSubAndAdd()).toBe(true)
     })
     it('should return true when - not found',()=>{
         let op = new Operator('5+2');
