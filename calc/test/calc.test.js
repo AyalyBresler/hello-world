2267efb37b7249f08b919eb8c4e6a12cecd81323
ayala <ayallag@skyvar>
2024-09-24 10:15:46 +0300

    add test

--- calc/test/calc.test.js
+++ calc/test/calc.test.js
@@ -6,19 +6,19 @@ describe('CALC', () => {
     it('should create numbers and operator in calc', () => {
         let calc = new Calculator('3+5');
         calc.calc();
-        expect(calc.num1).toBe(3);
-        expect(calc.operator).toBe('+');
-        expect(calc.num2).toBe(5);
+        expect(calc.num1).toEqual(3);
+        expect(calc.operator).toEqual('+');
+        expect(calc.num2).toEqual(5);
     })
     it('should create num1 with the long exercise', () => {
         let calc = new Calculator('2+3+5*2');
         calc.findNum1;
-        expect(calc.num1).toBe(5);
+        expect(calc.num1).toEqual(5);
     })
     it('should create num2 with the long exercise after', () => {
         let calc = new Calculator('3*5+8+6');
         calc.findNum2;
-        expect(calc.num2).toBe(5);
+        expect(calc.num2).toEqual(5);
     })
     it('should fill the num1 according to the exercise', () => {
         let calc = new Calculator('5+3+5*6')
@@ -31,25 +31,25 @@ describe('CALC', () => {
         let op = new Operator(calc.str.substring(0, this.op));
         calc.num1SetIndex(op);
         expect(calc.num1BeginIndex).toBe(0);
-        expect(num1).toBe(5)
+        expect(num1).toEqual(5)
     })
     it('should return 0 when there is not more than one operator before', () => {
         let calc = new Calculator('5+6');
         let op = new Operator(calc.str.substring(0, this.op));
         let result = calc.num1IndexOp(op);
-        expect(result).toBe(0)
+        expect(result).toEqual(0)
     })
     it('should return the index of operator when there is more than one operator before', () => {
         let calc = new Calculator('5+6*2');
         let op = new Operator(calc.str.substring(0, this.op));
         let result = calc.num1IndexOp(op);
-        expect(result).toBe(2)
+        expect(result).toEqual(2)
     })
     it('should return the number of num1 even if there is more than one operator', () => {
         let calc = new Calculator('5+6*2');
         let op = new Operator(calc.str.substring(0, this.op));
         calc.num1IndexOp(op);
         let result = calc.num1ReturnNumber();
-        expect(result).toBe(6)
+        expect(result).toEqual(6)
     })
 })
