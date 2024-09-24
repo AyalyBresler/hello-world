4a3cbab889b4fce8dc84d83e38754fd6dcd7768f
ayala <ayallag@skyvar>
2024-09-24 10:32:53 +0300

    add test

--- calc/test/calc.test.js
+++ calc/test/calc.test.js
@@ -18,37 +18,36 @@ describe('CALC', () => {
     it('should create num2 with the long exercise after', () => {
         let calc = new Calculator('3*5+8+6');
         calc.findNum2;
-        let result = calc.num2;
-        expect(result).toEqual(5);
+        expect(calc.num2).toEqual(5);
     })
     it('should return false when send operator before num1', () => {
         let calc = new Calculator('5+5*6')
-        let op = new Operator(calc.str.substring(0, this.op));
+        let op = new Operator(calc.str.substring(0, calc.op));
         let result = calc.num1IsNotANumber(op)
         expect(result).toBe(false);
     })
     it('should return there is operator before * operator', () => {
         let calc = new Calculator('5+6');
-        let op = new Operator(calc.str.substring(0, this.op));
+        let op = new Operator(calc.str.substring(0, calc.op));
         calc.num1SetIndex(op);
         expect(calc.num1BeginIndex).toBe(0);
         expect(calc.num1).toEqual(5)
     })
     it('should return 0 when there is not more than one operator before', () => {
         let calc = new Calculator('5+6');
-        let op = new Operator(calc.str.substring(0, this.op));
+        let op = new Operator(calc.str.substring(0, calc.op));
         let result = calc.num1IndexOp(op);
         expect(result).toEqual(0)
     })
     it('should return the index of operator when there is more than one operator before', () => {
         let calc = new Calculator('5+6*2');
-        let op = new Operator(calc.str.substring(0, this.op));
+        let op = new Operator(calc.str.substring(0, calc.op));
         let result = calc.num1IndexOp(op);
         expect(result).toEqual(2)
     })
     it('should return the number of num1 even if there is more than one operator', () => {
         let calc = new Calculator('5+6*2');
-        let op = new Operator(calc.str.substring(0, this.op));
+        let op = new Operator(calc.str.substring(0, calc.op));
         calc.num1IndexOp(op);
         let result = calc.num1ReturnNumber();
         expect(result).toEqual(6)
